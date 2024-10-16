43179a1ca35730c3e8495704daebf4741a279b56
Shraddhaw4 <134039137+Shraddhaw4@users.noreply.github.com>
2024-10-16 16:31:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,6 @@ provider "aws" {
   region                   = "ap-south-1"
 }
 
-variable "aws_access_key" { 
-}
-
-variable "aws_secret_key" {
-}
-
 resource "aws_instance" "test" {
   ami = "ami-04a37924ffe27da53"
   instance_type = "t2.micro"
